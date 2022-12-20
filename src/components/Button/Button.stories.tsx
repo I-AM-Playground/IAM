9a844411fbce71b87431d80de12c70d7389e358c
OrangeChick <retww@naver.com>
2022-12-20 23:51:41 +0900

    test: ✅ Add Button test code
    - Next Button 관련 storybook text code를 추가하였습니다.
    - Login Button 관련 storybook text code를 추가하였습니다.

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,11 @@
+import {
+  ArrowLongRightIcon,
+  EnvelopeIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
 import type { Meta, StoryFn } from '@storybook/react';
-
 import { Button } from '.';
+import { Icon } from '../Icon';
 
 const meta = {
   component: Button,
@@ -18,4 +23,32 @@ export default meta;
 
 export const Template: StoryFn<typeof Button> = ({ ...args }) => (
   <Button {...args}>{args.text}</Button>
+);
+
+export const Next: StoryFn<typeof Button> = ({ ...args }) => (
+  <Button className="w-[400px]" {...args}>
+    로그인 하러 가기
+    <Icon icon={ArrowLongRightIcon} className="ml-5 h-6 w-6 text-white" />
+  </Button>
+);
+
+export const LoginKakao: StoryFn<typeof Button> = ({ ...args }) => (
+  <Button className="bg-kakao-yellow px-8 text-black" {...args}>
+    <Icon icon={UserIcon} className="m-4 h-[50px] w-[50px]" />
+    <Button.Title>카카오 계정으로 로그인하기</Button.Title>
+  </Button>
+);
+
+export const LoginGoogle: StoryFn<typeof Button> = ({ ...args }) => (
+  <Button className="border border-red bg-white px-8 text-black" {...args}>
+    <Icon icon={UserIcon} className="m-4 h-[50px] w-[50px]" />
+    <Button.Title>구글 계정으로 로그인하기</Button.Title>
+  </Button>
+);
+
+export const LoginEmail: StoryFn<typeof Button> = ({ ...args }) => (
+  <Button className="bg-red px-8" {...args}>
+    <Icon icon={EnvelopeIcon} className="m-4 h-[50px] w-[50px] text-white" />
+    <Button.Title>이메일로 로그인하기</Button.Title>
+  </Button>
 );
