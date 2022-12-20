08379347b97bb346f147e2266d73c1d102864b18
OrangeChick <retww@naver.com>
2022-12-20 17:41:36 +0900

    test: ✅ Add MobileNav test code

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryFn } from '@storybook/react';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+import { EllipsisHorizontalIcon, HomeIcon } from '@heroicons/react/24/outline';
 import { IconButton } from './IconButton';
 
 const meta = {
@@ -11,6 +11,7 @@ const meta = {
       url: 'figmaURL',
     },
   },
+  args: { title: 'Home' },
 } as Meta<typeof IconButton>;
 
 export default meta;
@@ -18,5 +19,12 @@ export default meta;
 export const Template: StoryFn<typeof IconButton> = (args) => (
   <IconButton {...args}>
     <IconButton.Icon icon={EllipsisHorizontalIcon} className="h-10 w-10" />
+  </IconButton>
+);
+
+export const MobileNav: StoryFn<typeof IconButton> = ({ ...args }) => (
+  <IconButton className="flex-col items-center justify-center" {...args}>
+    <IconButton.Icon icon={HomeIcon} className="h-10 w-10" />
+    <IconButton.Title>{args.title}</IconButton.Title>
   </IconButton>
 );
